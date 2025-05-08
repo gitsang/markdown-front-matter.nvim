c6c4769241910ad8742ea8bfa682a265f01507d9
sang <sang.chen@outlook.com>
2025-05-08 15:20:23 +0800

    Add init.lua to redirect plugin loading

---
+++ init.lua
@@ -0,0 +1,3 @@
+-- This file is a simple redirect to the actual plugin implementation
+-- It allows the plugin to be loaded both as a regular plugin and as a module
+return require("markdown-front-matter")
